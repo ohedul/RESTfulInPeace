604b3396bd0343ed87da3dcedd1f443cf9f0c0f1
Md Ohedul Alam <ohedulalam@outlook.com>
2019-01-21 14:33:49 +0600

    some commit

--- RESTfullInPeace/src/main/java/rokomari/project/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package rokomari.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import rokomari.project.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-
-}
